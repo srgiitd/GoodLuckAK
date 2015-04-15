26163a229ee94c02b9f5f0ecbaae8d549f2122e0
srgiitd <gupta.suresh@gmail.com>
2015-04-15 11:00:31 +0200

    Update core.clj

--- src/GoodLuckAK/core.clj
+++ src/GoodLuckAK/core.clj
@@ -4,4 +4,4 @@
 (defn -main
   [& args]
   (println "It was a great pleasure to work with you. Good luck and all the best in the future!")
-  (println "Mircea & Zsolt"))
+  (println "Mircea, Zsolt & Suresh"))
